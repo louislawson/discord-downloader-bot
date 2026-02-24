466540710a7c9f844a699f767814b112de72fec4
Louis Lawson <louis.lawson@louislawson.co.uk>
2026-02-24 16:12:00 +0000

    removed debug logging

--- cogs/download.py
+++ cogs/download.py
@@ -36,17 +36,12 @@ class Download(commands.Cog, name="download"):
         await context.defer()
         attachments: List[Tuple[str, BytesIO]] = []
         async for message in context.channel.history(limit=None):
-            self.bot.logger.info(message.id)
             for attachment in message.attachments:
-                self.bot.logger.info(attachment.filename)
-                self.bot.logger.info(attachment.content_type)
-                self.bot.logger.info(attachment.size)
                 if self.is_allowed_media_type(attachment.content_type):
                     att_buffer = BytesIO()
                     await attachment.save(att_buffer)
                     att_buffer.seek(0)
                     attachments.append((attachment.filename, att_buffer))
-            self.bot.logger.info("----------------------------")
 
         zip_buffer = BytesIO()
         with ZipFile(zip_buffer, mode="w", compression=ZIP_DEFLATED) as zip_file:
