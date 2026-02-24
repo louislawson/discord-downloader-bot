a46e51f433bfe494ddd8c01f42f427e5007c286e
Louis Lawson <louis.lawson@louislawson.co.uk>
2026-02-24 15:59:29 +0000

    removed file logger

--- bot.py
+++ bot.py
@@ -59,16 +59,9 @@ logger.setLevel(logging.INFO)
 # Console handler
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(LoggingFormatter())
-# File handler
-file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-file_handler_formatter = logging.Formatter(
-    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
-)
-file_handler.setFormatter(file_handler_formatter)
 
 # Add the handlers
 logger.addHandler(console_handler)
-logger.addHandler(file_handler)
 
 
 class DiscordBot(commands.Bot):
